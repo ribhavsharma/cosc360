5c925288ddb04f49f28c4c404df2cad8bd8fecde
OM200401 <ommistry012004@gmail.com>
2024-03-23 19:38:42 -0700

    made changes to DB config and connection files

--- app/core/connection.php
+++ app/core/connection.php
@@ -1,9 +1,9 @@
 <?php
 
 if($_SERVER['SERVER_NAME'] == 'localhost'){
-    define('DBUSER', "root");
-    define("DBPASS", "");
-    define("DBNAME", "myblogpost");
+    define('DBUSER', "76297597");
+    define("DBPASS", "76297597");
+    define("DBNAME", "db_76297597");
     define("DBHOST", "localhost");
 }else{
     define('DBUSER', "root");
