be0de6862c436a9508b345fa65110a3583284839
aamirahmed2004 <syedaamir099@gmail.com>
2024-04-11 10:21:08 -0700

    Added tests for home, login, posts, and admin

---
+++ cypress/e2e/admin.cy.js
@@ -0,0 +1,30 @@
+describe('Admin Dashboard Test', () => {
+  beforeEach(() => {
+    cy.visit('https://localhost/cosc360%20-%20Copy/app/pages/login.php'); 
+
+    cy.get('.flip-card__front form input[name="email"]').type('aamir@gmail.com');
+    cy.get('.flip-card__front form input[name="password"]').type('12345678');
+    cy.get('.flip-card__front form button[type="submit"]').click();
+  }); 
+
+  it('admin dashboard page loads and has the expected elements', () => {
+    cy.visit("https://localhost/cosc360%20-%20Copy/app/pages/admin.php?section=dashboard")
+
+    // Check that the page contains some expected text
+    cy.contains('Dashboard')
+    cy.contains('Admins')
+    cy.contains('Users')
+    cy.contains('Categories')
+    cy.contains('Posts')
+
+    // Check for specific elements
+    cy.get('.navbar').should('exist') 
+    cy.get('.sidebar').should('exist') 
+    cy.get('.breadcrumb').should('exist') 
+
+    cy.get('button').contains('Share').should('exist')
+    cy.get('button').contains('Export').should('exist')
+
+    cy.get('a').contains('Sign out').should('exist')
+  })
+})
