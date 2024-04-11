cdbb1e7209e86765eafcd3eb962503a5a1b18451
OM200401 <ommistry012004@gmail.com>
2024-04-11 13:46:46 -0700

    ROOT defined again

--- app/core/config.php
+++ app/core/config.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('ROOT')) {
-    define('ROOT', "https://cosc360.ok.ubc.ca/app/public");
+    define('ROOT', "https://cosc360.ok.ubc.ca/om12345/app/public");
 }
 
 if (!defined('APP_NAME')) {
